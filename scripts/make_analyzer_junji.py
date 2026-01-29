18d0cd9545cadce272e617eaba0bcabd02064839
chrishalcrow <chrishalcrow@gmail.com>
2026-01-29 12:39:04 +0000

    dont load clusters

--- scripts/make_analyzer_junji.py
+++ scripts/make_analyzer_junji.py
@@ -8,10 +8,7 @@ analyzer_output_path = f"sub-{mouse}_day-{day}_ses-{session_type}_analyzer"
 
 session = JunjiSession(mouse, day, session_type, active_projects_path="/run/user/1000/gvfs/smb-share:server=cmvm.datastore.ed.ac.uk,share=cmvm/sbms/groups/CDBS_SIDB_storage/NolanLab/ActiveProjects/")
 
-print("Loading data...")
-clusters = session.get_clusters()
-
-print("Making analyzer...")
+# print("Making analyzer...")
 analyzer = session.create_analyzer()
 
 print("Saving analyzer...")
